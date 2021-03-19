663c7ca39f846d229076521e913fa782e9d931e4
yanmun <hym7743@gmail.com>
2021-03-19 22:43:45 +0800

    login process

---
+++ Controller/login_process.php
@@ -0,0 +1,21 @@
+<?php
+
+include("sql.php");
+$mysqli = GETSQLLink();
+$email= $_POST['email'];
+$password = $_POST['password'];
+
+$query = "select * from user where email = '$email' and password ='$password'";
+$res = $mysqli->query($query);
+$row = $res->fetch_object();
+$mysqli -> close();
+
+if(!$row){
+
+    header("Location: Login.php");
+
+}else{
+    header("Location: ../UI/matchmaking.html");
+}
+
+?>
