dc4b4b2f872cf7995873758e715f4caeaf75ebc3
k4l4p <kalapwong@gmail.com>
2021-04-14 16:31:32 +0800

    Update database

--- DB/sakura.sql
+++ DB/sakura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 12, 2021 at 02:29 PM
+-- Generation Time: Apr 14, 2021 at 08:31 AM
 -- Server version: 10.5.9-MariaDB
 -- PHP Version: 8.0.3
 
@@ -20,6 +20,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `sakura`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chatid` int(11) NOT NULL,
+  `sender_userid` int(11) NOT NULL,
+  `reciever_userid` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`chatid`, `sender_userid`, `reciever_userid`, `message`, `timestamp`, `status`) VALUES
+(1, 1, 0, 'hi', '2021-04-14 08:24:19', 1),
+(2, 1, 0, 'hi', '2021-04-14 08:24:36', 1),
+(3, 1, 0, 'aa', '2021-04-14 08:25:15', 1),
+(4, 1, 2, 'hi', '2021-04-14 08:26:16', 1),
+(5, 1, 2, 'haha', '2021-04-14 08:26:26', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_login_details`
+--
+
+CREATE TABLE `chat_login_details` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `last_activity` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_typing` enum('no','yes') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,7 +81,8 @@ CREATE TABLE `preferences` (
 --
 
 INSERT INTO `preferences` (`userID`, `pref1`, `pref2`, `pref3`, `pref4`, `image`, `description`) VALUES
-(1, 1, 1, 1, 1, 'uploadimages/00000000.jpg', 'fdfsadf');
+(1, 3, 2, 1, 4, 'uploadimages/00000000.jpg', 'hahahaha\r\n                                    '),
+(2, 3, 3, 1, 4, 'uploadimages/00000000.jpg', 'Hi I am tom\r\n                                    ');
 
 -- --------------------------------------------------------
 
@@ -54,19 +94,30 @@ CREATE TABLE `user` (
   `userID` int(11) NOT NULL,
   `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `online` tinyint(1) NOT NULL DEFAULT 0,
+  `current_session` int(11) NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userID`, `username`, `password`, `email`) VALUES
-(1, 'sakura', 'sakura', 'sakura@email.com');
+INSERT INTO `user` (`userID`, `username`, `password`, `email`, `online`, `current_session`, `avatar`) VALUES
+(1, 'sakura', 'sakura', 'sakura@email.com', 1, 2, 'uploadimages/00000000.jpg'),
+(2, 'tom', '123456', 'tom@email.com', 0, 0, 'uploadimages/00000000.jpg'),
+(3, 'mary', '123456', 'mary@email.com', 0, 0, 'uploadimages/00000000.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatid`);
 
 --
 -- Indexes for table `user`
@@ -79,10 +130,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
